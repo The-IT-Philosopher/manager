0270a8684c03347f54e7da1a61072549c6280edc
André van Schoubroeck <andre@philosopher.it>
2015-10-11 12:29:01 +0200

    VAT validation

--- components/Customer/Customer.php
+++ components/Customer/Customer.php
@@ -34,6 +34,7 @@ class Customer {
         if (isset($_POST['country']) && $_POST['country']=='NL') {
           $_SESSION['CustomerAddWizard']['page']+=2;
           $_SESSION['CustomerAddWizard']['OrganisationCountry']="NL";
+          $_SESSION['CustomerAddWizard']['OrganisationRegion']="EU";
         } else if (isset($_POST['region']) && in_array($_POST['region'],array("EU","world")) ) {
           $_SESSION['CustomerAddWizard']['OrganisationRegion']=$_POST['region'];
           $_SESSION['CustomerAddWizard']['page']++;
@@ -84,7 +85,17 @@ class Customer {
           $insertData[':person_last_name'] = $_POST['last_name'];
           $sth = $pdo->prepare("INSERT INTO person (person_first_name, person_initials, person_last_name_prefix, person_last_name) VALUES (:person_first_name, :person_initials, :person_last_name_prefix, :person_last_name)");
           $sth->execute($insertData);
-          $_SESSION['CustomerAddWizard']['personId'] = $pdo->lastInsertId();
+          $person_id = $pdo->lastInsertId();
+          $_SESSION['CustomerAddWizard']['personId'] = $person_id;
+          if (strlen($_POST['email_address'])) {
+            $insertData = array();
+            $insertData[":email_verification"]=sha1(mcrypt_create_iv(16)); 
+            $insertData[":email_address"]=$_POST['email_address'];
+            $sth = $pdo->prepare("INSERT INTO email (email_address,email_verification) VALUES (:email_address,:email_verification)");
+            $sth->execute($insertData);
+            $email_id = $pdo->lastInsertId();
+            $_SESSION['CustomerAddWizard']['emailId'] = $email_id;
+          }
           $_SESSION['CustomerAddWizard']['page']++;
         } else {
 
@@ -94,6 +105,7 @@ class Customer {
           $data['content_raw'] .= "<tr><td>Voorletters</td><td><input type=text name=initials></td></tr>";
           $data['content_raw'] .= "<tr><td>Tussenvoegsel</td><td><input type=text name=last_name_prefix></td></tr>";
           $data['content_raw'] .= "<tr><td>Achternaam</td><td><input type=text name=last_name></td></tr>";
+          $data['content_raw'] .= "<tr><td>E-mail adres</td><td><input type=email name=email_address></td></tr>";
           $data['content_raw'] .= "<tr><td></td><td><input type=submit value=volgense></td></tr>";
           $data['content_raw'] .= "</table></form>";
           break;
@@ -127,7 +139,7 @@ class Customer {
                 $insertData[':address_city']= $_SESSION['CustomerAddWizard']['KvKdata']['address_city'];
                 $sth->execute($insertData);
                 $address_id = $pdo->lastInsertId(); 
-                
+                $_SESSION['CustomerAddWizard']['addressId'] = $address_id; 
                 $sth = $pdo->prepare("INSERT INTO organisation (organisation_name, organisation_type, organisation_nl_kvk, organisation_country)
                                       VALUES (:organisation_name, :organisation_type, :organisation_nl_kvk, 'NL')");
 
@@ -137,7 +149,7 @@ class Customer {
                 $insertData[':organisation_nl_kvk'] = $kvkData['kvk_nummer'];
                 $sth->execute($insertData);
                 $organisation_id = $pdo->lastInsertId();
-
+                $_SESSION['CustomerAddWizard']['organisationId'] = $organisation_id;
                 $sth = $pdo->prepare ("INSERT INTO link_address2organisation (address_id, organisation_id, address_type) VALUES
                                        (:address_id, :organisation_id, 'validated' )");
                 $sth->execute(array(":address_id"=>$address_id, ":organisation_id" => $organisation_id));
@@ -167,9 +179,48 @@ class Customer {
          break;
        }
      case 7:
-      $data['content_raw'] .= "VAT NUMBER";
+      if ($_SESSION['CustomerAddWizard']['OrganisationRegion']=="EU") {
+
+        //TODO check organisation type to see if VAT is required!
+
+        $vat_valid = false;
+        if (isset($_POST['vat'])) {
+          require_once("components/vat-validation/vatValidation.class.php");
+          $vat_number = strtoupper($_POST['vat']); // capitalise string
+          $vat_number = str_replace(' ', '', $vat_number); // remove spaces
+          if (substr($vat_number,0,2)!=$_SESSION['CustomerAddWizard']['OrganisationCountry']) {
+            //VAT Number entered without country prefix
+            $vat_number = $_SESSION['CustomerAddWizard']['OrganisationCountry'] . $vat_number;
+          }
+                                 
+            $vat_validator = new vatValidation();
+            $vat_valid = $vat_validator->check( $_POST['vat'] );
+            if ($vat_valid) {
+              $vatData = $vat_validator->getData();
+              //$data['content_raw'] .= "<pre>VIES DATA\n" . var_export($vatData,true) . "</pre>";
+              $sth = $pdo->prepare("UPDATE organisation 
+                                    SET    organisation_vat = :organisation_vat 
+                                    WHERE  organisation_id  = :organisation_id");
+              $sth->execute(array(":organisation_vat" => $_POST['vat'], "organisation_id" => $_SESSION['CustomerAddWizard']['organisationId'] ));
+              $_SESSION['CustomerAddWizard']['page']++;
+            }        
+        
+        }
+        if (!$vat_valid) {
+         $data['content_raw'] .= "Belastinginformatie";
+         $data['content_raw'] .= "<form method=post>";
+         $data['content_raw'] .= "<table>";
+         $data['content_raw'] .= "<tr><td>BTW Nummer</td><td><input type=text name=vat></td></tr>";
+         $data['content_raw'] .= "<tr><td></td><td><input type=submit value=volgense></td></tr>";
+         $data['content_raw'] .= "</table></form>";
+         break;
+       }
+     } else {
+       //VAT number only applicable to EU countries
+       $_SESSION['CustomerAddWizard']['page']++;
+     }  
      case 8:
-      $data['content_raw'] .= "done?";
+      $data['content_raw'] .= "done";
 
     }
   }
